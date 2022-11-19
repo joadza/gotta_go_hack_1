8f8e23a6eedbd8787e37e5412c61617f7982be88
Nohoarii <famibellenohoarii@hotmail.com>
2022-11-19 18:20:17 +0100

    Premier commit

---
+++ Adam/asset/js/script.js
@@ -0,0 +1,11 @@
+const barCanvas = document.getElementById('barChart');
+const barChart = new Chart(barCanvas, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            data: [12, 19, 3, 5, 2, 3],
+            
+        }]
+    }
+});
